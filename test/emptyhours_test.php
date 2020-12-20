13751103bbaabab2dbfa8347942aa641e0338271
Wolf <72465439+verawolf94@users.noreply.github.com>
2020-12-19 23:03:44 -0800

    Create emptyhours_test.php

---
+++ test/emptyhours_test.php
@@ -0,0 +1,22 @@
+<?php
+//emptyhours_test.php
+include_once 'includes/settings.php';
+require_once 'simpletest/autorun.php';
+require_once 'simpletest/web_tester.php';
+
+class EmptyHoursForm extends WebTestCase {
+
+      function testCorrectData() {
+        $this->get(VIRTUAL_PATH . '/hours.php');
+        $this->assertResponse(200);
+
+    $this->setField("hours", "");
+        $this->setField("rate", "");
+        $this->clickSubmit("Show Pay");
+
+        $this->assertResponse(200);
+        $this->assertText("Please fill out this field.
+");
+    }
+
+}
